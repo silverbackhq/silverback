6c506dd0788b920310a62e2794290d367b25295a
Clivern <hello@clivern.com>
2018-12-29 21:24:24 +0100

    add Register_Request model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -7,6 +7,7 @@ from .option import Option                                                      
 from .profile import Profile                                                      # noqa: F401
 from .user_meta import User_Meta                                                  # noqa: F401
 from .reset_request import Reset_Request                                          # noqa: F401
+from .register_request import Register_Request                                    # noqa: F401
 from .task import Task                                                            # noqa: F401
 from .notification import Notification                                            # noqa: F401
 from .subscriber import Subscriber                                                # noqa: F401
---
+++ app/models/register_request.py
@@ -0,0 +1,15 @@
+"""
+Register Request Model
+"""
+
+# Django
+from django.db import models
+
+
+class Register_Request(models.Model):
+
+    email = models.CharField(max_length=60, db_index=True, verbose_name="Email")
+    token = models.CharField(max_length=200, db_index=True, verbose_name="Token")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated at")
+    expire_at = models.DateTimeField(verbose_name="Expire at")
