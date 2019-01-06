fa01cc7ada45d40ee5d801882e683152bf82a352
Clivern <hello@clivern.com>
2019-01-06 14:34:46 +0100

    Update metric & activity entity

--- app/models/metric.py
+++ app/models/metric.py
@@ -8,7 +8,13 @@ from django.db import models
 
 class Metric(models.Model):
 
-    name = models.CharField(max_length=100, verbose_name="Name")
-    description = models.CharField(max_length=200, verbose_name="Description")
+    TYPE_CHOICES = (
+        ('graphite', 'GRAPHITE'),
+        ('prometheus', 'PROMETHEUS')
+    )
+
+    title = models.CharField(max_length=200, verbose_name="Title")
+    type = models.CharField(max_length=50, choices=TYPE_CHOICES, default="graphite", verbose_name="Type")
+    source = models.TextField(verbose_name="Source")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated at")
--- app/modules/entity/activity_entity.py
+++ app/modules/entity/activity_entity.py
@@ -2,6 +2,58 @@
 Activity Entity Module
 """
 
+from django.contrib.auth.models import User
+from app.models import Activity
+
 
 class Activity_Entity():
-    pass
+
+    def insert_one(self, activity):
+
+        new_activity = Activity()
+
+        if "activity" in activity:
+            new_activity.activity = activity["activity"]
+
+        if "user_id" in activity:
+            new_activity.user = User.objects.get(pk=activity["user_id"])
+
+        new_activity.save()
+        return False if new_activity.pk is None else new_activity
+
+    def update_one_by_id(self, id, activity_data):
+        activity = self.get_one_by_id(id)
+
+        if activity is not False:
+            if "activity" in activity_data:
+                activity.activity = activity_data["activity"]
+
+            if "user_id" in activity_data:
+                activity.user = User.objects.get(pk=activity_data["user_id"])
+
+            activity.save()
+            return True
+        return False
+
+    def count_all(self):
+        return Activity.objects.count()
+
+    def get_all(self, offset=None, limit=None):
+        if offset is None or limit is None:
+            return Activity.objects.order_by('-created_at').get()
+
+        return Activity.objects.order_by('-created_at')[offset:limit+offset]
+
+    def get_one_by_id(self, incident_id):
+        try:
+            activity = Activity.objects.get(id=incident_id)
+            return False if activity.pk is None else activity
+        except Exception:
+            return False
+
+    def delete_one_by_id(self, id):
+        activity = self.get_one_by_id(id)
+        if activity is not False:
+            count, deleted = activity.delete()
+            return True if count > 0 else False
+        return False
