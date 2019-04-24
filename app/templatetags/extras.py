4720dcd6161e0602ca20e25a2df0e92539aec4fa
Clivern <hello@clivern.com>
2019-04-24 16:47:17 +0200

    add sample template tags

---
+++ app/templatetags/extras.py
@@ -0,0 +1,18 @@
+"""
+Template Extra Tags
+
+{% load extras %}
+
+{{ somevariable|cut:"0" }}
+"""
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter(name='cut')
+def cut(value, arg):
+    """Removes all values of arg from the given string"""
+    return value.replace(arg, '')
