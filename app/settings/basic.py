4f7ef78801c11348fd95d44eef3a16c83ac1ab92
Clivern <hello@clivern.com>
2019-07-05 20:34:28 +0200

    Fix

--- app/settings/basic.py
+++ app/settings/basic.py
@@ -269,8 +269,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_URL = '/static/'
 
-STATIC_ROOT = APP_ROOT + STATIC_URL
-
 STATICFILES_DIRS = [
     APP_ROOT + STATIC_URL
 ]
