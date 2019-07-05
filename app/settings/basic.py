622a0dbc757f959705dd0252105f8b28ef00cc14
Clivern <hello@clivern.com>
2019-07-06 00:06:56 +0200

    new fix

--- app/settings/basic.py
+++ app/settings/basic.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'app.middleware.correlation.Correlation',
     'app.middleware.api_funnel.APIFunnel',
     'app.middleware.web_funnel.WebFunnel',
