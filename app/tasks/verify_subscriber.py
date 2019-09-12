b5979c43a97fc523bd616d0cdd5e6f5da5311e7c
Clivern <hello@clivern.com>
2019-09-12 20:25:04 +0200

    add verify task

--- app/tasks/verify_subscriber.py
+++ app/tasks/verify_subscriber.py
@@ -5,17 +5,48 @@ Verify Subscriber Tasks
 # Third Party Library
 from celery import shared_task
 
+# Local Library
+from app.modules.entity.option_entity import OptionEntity
+from app.modules.core.subscriber import Subscriber as SubscriberModule
+
 
 @shared_task
-def verify_email():
+def verify_subscriber(subscriber_id):
+
+    # option_entity = OptionEntity()
+    subscriber_module = SubscriberModule()
+
+    # app_name = option_entity.get_value_by_key("app_name")
+    # app_email = option_entity.get_value_by_key("app_email")
+    # app_url = option_entity.get_value_by_key("app_url")
+    subscriber = subscriber_module.get_one_by_id(subscriber_id)
+
+    if not subscriber:
+        return {
+            "status": "passed",
+            "result": "{}"
+        }
+
+    if subscriber.type == SubscriberModule.EMAIL:
+        result = __verify_email()
+    elif subscriber.type == SubscriberModule.PHONE:
+        result = __verify_phone()
+    elif subscriber.type == SubscriberModule.ENDPOINT:
+        result = __verify_endpoint()
+
+    return result
+
+
+@shared_task
+def __verify_email():
     pass
 
 
 @shared_task
-def verify_phone():
+def __verify_phone(to, ):
     pass
 
 
 @shared_task
-def verify_endpoint():
+def __verify_endpoint():
     pass
